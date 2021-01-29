4e63ab642b11c04643dc70009aefe7c63835bd82
Abel program <Abel program@DESKTOP-IPN9FJV.lan>
2021-01-29 11:17:14 +0100

    skapat kundens meny

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -1,17 +1,66 @@
-/*
-Authors: Cecilia Carboo & Linda Youkhanna Esefzara JAVA20, January2021
- */
+
 package hotel;
 
-public class Hotel {
+import java.sql.Connection;
+import java.util.*;
 
+
+
+
+public class Hotel{
+
+    private static Scanner input = new Scanner(System.in);
+    //private static Connection connection = null;
+    private static boolean exit = false;
+    
+    
+    
+    
     public static void main(String[] args) {
+      
+                
+        
+        // Customer view(skapa en meny för att välja mellan olika alternativ):
+        while (!exit) {
 
-        Room room1 = new Room();
+                System.out.println("Make your choice!");
+                System.out.println("1. Display room details");
+                System.out.println("2. Display room availability");
+                System.out.println("3. Book");
+                System.out.println("4. Order food");
+                System.out.println("5. Checkout");
+                System.out.println("7. Exit1");
+
+                int selection = input.nextInt();
+
+                switch (selection) {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                       
+                        break;
+                    case 4:
+                       
+                        break;
+                    case 5:
+                        
+                        break;
+                    case 6:
+                        
+                        break;
+                    default:
+                        exit = true;
+                }
+            }
+
+
+               
         
-        room1.setRoomID(1);
         
-        System.out.println("Room nr 1: " + room1);
-    }
+}
     
 }
