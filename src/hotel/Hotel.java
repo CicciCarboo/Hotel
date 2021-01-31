b40a2087ff492ac318dcc82fee28b2e10a891481
cicci <cicci@Cicci-Laptop>
2021-01-31 17:05:46 +0100

    Room class: bookRoom menu with collection Set, streams and lambda expressions done.

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -13,25 +13,7 @@ public static void main(String[] args) {
                       
        Room.generateHotel();
        
-       
-    Menu m = new Menu();
-        System.out.println("Menu class" + m);
-
-    Food f = new Food();
-        System.out.println("class food:" + f);
-    
-    Noodles n = new Noodles();
-        System.out.println("class noodles:" + n);
-        
-    PastaRs pr = new PastaRs();
-        System.out.println("class PastaRs:" + pr);
-        
-    Sandwich s = new Sandwich();
-        System.out.println("class Sandwich:" + s);
-        
-    Drink d = new Drink();
-        System.out.println("class Drink:" + d);
-        
+      
 
 
 }
